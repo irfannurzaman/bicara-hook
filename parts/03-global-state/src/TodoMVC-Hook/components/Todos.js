6002e75215334c056670ba0f7a89c75d5d62715a
anitalim <anita.lim@hotmail.com>
2020-10-06 09:27:33 +0700

    hook

---
+++ parts/03-global-state/src/TodoMVC-Hook/components/Todos.js
@@ -0,0 +1,27 @@
+import React,{useContext} from 'react';
+import Todo from './Todo';
+import { update, remove } from '../actions';
+import Nodux from "../Nodux";
+
+function Todos(){
+  const [state, dispatch] = useContext(Nodux.AppContext);
+  console.log('sukses2', state)
+   const  onRemove = (id) => dispatch(remove(id));
+   const  onCompleted  = (event,id) => {
+    const checked = event.target.checked; 
+    dispatch(update(id, checked));
+  }
+  
+  return state.map(todo => (
+    <Todo
+      key={todo.id}
+      id={todo.id}
+      todo={todo.todo}
+      completed={todo.completed}
+      onRemove={onRemove}
+      onCompleted={onCompleted}
+    />
+  ));
+}
+
+export default Todos;
